cc5bc5f8f50130605134232552d23da69d0320b5
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-10-23 14:47:49 +0600

    Add: gell all user with search and filtering

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -3,6 +3,8 @@ import catchAsync from "../../shared/catchAsync";
 import sendResponse from "../../shared/sendResponse";
 import { fileUploder } from "../../helper/fileUploder";
 import { AdminService } from "./admin.service";
+import { pick } from "../../helper/pick";
+import { userFilterableFields } from "./admin.constans";
 
 const createAdmin = catchAsync(async (req: Request, res: Response) => {
 
@@ -22,6 +24,22 @@ const createAdmin = catchAsync(async (req: Request, res: Response) => {
     })
 });
 
+const getAllUser = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, userFilterableFields);
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]) as any;
+    
+    const result = await AdminService.getAllUser(filters, options);
+
+    sendResponse(res,{
+        statusCode: 200,
+        success: true,
+        message:"User retrieved successfully!",
+        meta: result.meta,
+        data: result.data
+    })
+});
+
 export const AdminController={
-    createAdmin
+    createAdmin,
+    getAllUser
 }
