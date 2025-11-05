ff586610bb62e8ad69a16299253bc912c9300893
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-11-05 23:25:57 +0600

    Add: Doctor Specialties

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,40 +1,45 @@
-import express from 'express';
-import { UserRouter } from '../modules/user/user.routes';
-import { AuthRouter } from '../modules/auth/auth.routes';
-import { DoctorRouter } from '../modules/doctor/doctor.routes';
-import { AdminRouter } from '../modules/admin/admin.routes';
-import { ScheduleRouter } from '../modules/schedule/schedule.route';
-import { DoctorScheduleRoutes } from '../modules/doctorSchedule/doctorSchedule.routes';
+import express from "express";
+import { UserRouter } from "../modules/user/user.routes";
+import { AuthRouter } from "../modules/auth/auth.routes";
+import { DoctorRouter } from "../modules/doctor/doctor.routes";
+import { AdminRouter } from "../modules/admin/admin.routes";
+import { ScheduleRouter } from "../modules/schedule/schedule.route";
+import { DoctorScheduleRoutes } from "../modules/doctorSchedule/doctorSchedule.routes";
+import { SpecialtiesRoutes } from "../modules/specialties/specialties.routes";
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/user',
+    path: "/user",
     route: UserRouter,
   },
   {
-    path:"/auth",
-    route: AuthRouter
+    path: "/auth",
+    route: AuthRouter,
   },
   {
-    path:"/doctor",
-    route: DoctorRouter
+    path: "/doctor",
+    route: DoctorRouter,
   },
   {
-    path:"/admin",
-    route: AdminRouter
+    path: "/admin",
+    route: AdminRouter,
   },
   {
-    path:"/schedule",
-    route: ScheduleRouter
+    path: "/schedule",
+    route: ScheduleRouter,
   },
   {
-    path:"/doctorSchedule",
-    route: DoctorScheduleRoutes
-  }
+    path: "/doctorSchedule",
+    route: DoctorScheduleRoutes,
+  },
+  {
+    path: "/specialties",
+    route: SpecialtiesRoutes,
+  },
 ];
 
-moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
